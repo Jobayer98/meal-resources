61b6469f44799d999056ae893486d407b04ffa8a
Jobayer <jobayerrahman0220@gmail.com>
2023-03-01 10:41:18 +0600

    finish modal window

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-const loadData = (st) => {
-  searchText = st || " ";
+const loadData = async (arg) => {
+  searchText = arg || " ";
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayMeals(data.meals.slice(0, 6)));
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const loadDataUsingCat = async () => {
+  const data = await loadData();
+  displayMeals(data.meals.slice(0, 6));
 };
 
 const displayMeals = (meals) => {
@@ -12,6 +23,8 @@ const displayMeals = (meals) => {
   const card_container = document.getElementById("card_container");
   card_container.innerHTML = "";
   meals.forEach((meal) => {
+    // console.log(meal);
+    const { idMeal, strMeal, strMealThumb, strInstructions } = meal;
     const card = document.createElement("div");
     card.classList.add(
       "card",
@@ -27,21 +40,19 @@ const displayMeals = (meals) => {
             <figure class="w-[150%] lg:w-[120%]">
               <img
                 class="h-full rounded-md"
-                src="${meal.strMealThumb}"
+                src="${strMealThumb}"
                 alt="Movie"
               />
             </figure>
             <div class="card-body">
-              <h2 class="card-title text-[#403F3F] text-2xl font-bold">
-               ${meal.strMeal}
+              <h2 class="card-title text-[#403F3F] text-xl font-bold">
+               ${strMeal}
               </h2>
               <p class="text-[#706F6F] text-sm lg:text-md">
-                ${meal.strInstructions.slice(0, 100) + "..."}
+                ${strInstructions.slice(0, 100) + "..."}
               </p>
               <div class="card-actions justify-start">
-                <a class="text-[#FFC107] text-[18px] underline font-bold" href="#"
-                  >View Details</a
-                >
+                <label onclick="showModal(${idMeal})" for="my-modal" class="btn btn-warning font-bold normal-case">View Details</label>
               </div>
             </div>
     `;
@@ -49,11 +60,76 @@ const displayMeals = (meals) => {
   });
 };
 
-const searchMeal = () => {
+const loadDataUsingId = async (id) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const showModal = async (mealId) => {
+  const data = await loadDataUsingId(mealId);
+  const {
+    strMeal,
+    strMealThumb,
+    strCategory,
+    strArea,
+    strInstructions,
+    strYoutube,
+  } = data.meals[0];
+  const modalContainer = document.querySelector(".modal");
+  modalContainer.innerHTML = "";
+
+  const modalBox = document.createElement("div");
+  modalBox.classList.add("modal-box");
+
+  modalBox.innerHTML = `
+          <h3 class="font-bold text-xl">
+            ${strMeal}
+          </h3>
+          <hr>
+          <figure class="">
+              <img
+                class="h-full rounded-md"
+                src="${strMealThumb}"
+                alt="Movie"
+              />
+
+          <p class="py-4">
+            <span class="text-lg font-semibold">Category: </span>${strCategory}
+          </p>
+          <p class="py-4">
+            <span class="text-lg font-semibold">Area: </span>${strArea}
+          </p>
+          <p class="py-4">
+            <span class="text-lg font-semibold">Instructions: </span>${strInstructions}
+          </p>
+          <p class="py-4">
+            <span class="text-lg font-semibold">Youtube: </span>${strYoutube}
+          </p>
+
+          <div class="modal-action">
+            <label for="my-modal" class="btn normal-case bg-[#DC3545] text-slate-100 border-none">Close</label>
+          </div>
+  `;
+  modalContainer.appendChild(modalBox);
+};
+
+const searchMeal = async () => {
   const searchKey = document.getElementById("search_meal");
   const searchValue = searchKey.value;
 
-  loadData(searchValue);
+  try {
+    const data = await loadData(searchValue);
+    displayMeals(data.meals.slice(0, 6));
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 const seeAllMeal = async () => {
@@ -70,4 +146,4 @@ const seeAllMeal = async () => {
   }
 };
 
-loadData();
+loadDataUsingCat();
